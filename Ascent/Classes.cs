30505dc806958f9c29f802d2b4ef5eef134d4a8d
asr1 <alexandersrinehart@gmail.com>
2016-06-08 23:52:49 -0700

    Add rudimentary class map

---
+++ Ascent/Classes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+
+namespace Ascent
+{
+    //Because it's not a problem if you stick two reserved words together.
+    public enum charClasses {CLERIC, OUTLAW, ENGINEER, PIRATE, CONVICT, VIGILANTE};
+
+    public class Classes
+    {
+        public static Dictionary<charClasses, Color> colorMap = new Dictionary<charClasses, Color>();
+
+        //Populate the color mapping
+        public static void initialize()
+        {
+            colorMap.Add(charClasses.CLERIC, Color.White);
+            colorMap.Add(charClasses.OUTLAW, Color.Red);
+            colorMap.Add(charClasses.ENGINEER, Color.LawnGreen);
+            colorMap.Add(charClasses.PIRATE, Color.Purple);
+            colorMap.Add(charClasses.CONVICT, Color.Orange);
+            colorMap.Add(charClasses.VIGILANTE, Color.Blue);
+        }
+
+    }
+}
