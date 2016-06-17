b42e79221f1310bd80c7336c398dc16be8880b8c
Jake Long <jakeprogrammer@gmail.com>
2016-06-16 22:28:10 -0500

    Changed Texture.* to be initial case.

--- Ascent/Resources/Textures.cs
+++ Ascent/Resources/Textures.cs
@@ -12,18 +12,19 @@ namespace Ascent.Resources
 {
     class Textures
     {
-        public static Texture2D character;
-        public static Texture2D playerCircle;
-        public static Texture2D leftArrow;
-        public static Texture2D rightArrow;
+        public static Texture2D Character;
+        public static Texture2D PlayerCircle;
+
+        public static Texture2D LeftArrow;
+        public static Texture2D RightArrow;
 
 
         public static void load(ContentManager content)
         {
-            character = content.Load<Texture2D>("textures/character");
-            playerCircle = content.Load<Texture2D>("textures/dotWhite");
-            leftArrow = content.Load<Texture2D>("textures/LeftArrow");
-            rightArrow = content.Load<Texture2D>("textures/RightArrow");
+            Character = content.Load<Texture2D>("textures/character");
+            PlayerCircle = content.Load<Texture2D>("textures/dotWhite");
+            LeftArrow = content.Load<Texture2D>("textures/LeftArrow");
+            RightArrow = content.Load<Texture2D>("textures/RightArrow");
         }
     }
 }
